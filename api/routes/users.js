a4ed86aa8c12a97807f3e0f1580c23c705e56b25
Mason Hall <masoncajunsensation@gmail.com>
2025-07-19 12:08:44 -0400

    added bio

--- api/routes/users.js
+++ api/routes/users.js
@@ -120,6 +120,7 @@ router.post('/getProfile', validateJWTMiddleware, async (req, res, next) => {
                     'profile.displayName': 1, 
                     'profile.PFP': 1, 
                     questPosts: 1,
+                    'profile.bio': 1,
                     _id: 0
                 } 
             }
