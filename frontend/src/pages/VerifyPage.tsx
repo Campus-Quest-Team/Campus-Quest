027607aebd004316dfb70e8bff6ead528e81d9d5
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2025-07-21 23:15:35 -0400

    Refactor current email verification ui

--- frontend/src/pages/VerifyPage.tsx
+++ frontend/src/pages/VerifyPage.tsx
@@ -1,11 +1,64 @@
-import EmailVerify from "../components/EmailVerify.tsx";
+import { useState, useEffect } from 'react';
+import { buildPath } from './Path';
+import { jwtDecode } from 'jwt-decode';
+import type { UserPayload } from '../types/APITypes';
 
-const VerifyPage = () => {
+
+function VerifyPage() {
+    const [message, setMessage] = useState(''); //used to give user a response
+
+    useEffect(() => {
+        doEmailVerify();
+    }, []);
+
+    async function doEmailVerify(): Promise<void> {
+        //pull userId and JWT sent via email from query URL
+        const urlQuery = new URLSearchParams(document.location.search);
+        const packetObject = { userId: urlQuery.get("UserId"), jwtToken: urlQuery.get("Token") };
+        const packetJSON = JSON.stringify(packetObject);
+
+        if (packetObject.jwtToken === null || packetObject.userId === null) {
+            setMessage('Missing UserId or Token');
+            return;
+        }
+
+        //check that decoded field matches
+        const decodeToken = jwtDecode<UserPayload>(packetObject.jwtToken);
+
+        try {
+            const decodedUserId = decodeToken.userId;
+            const userId = packetObject.userId;
+
+            if (!(decodedUserId == userId)) {
+                setMessage('UserId and associated token do not match');
+                return;
+            }
+        } catch (error: any) {
+            alert(error.toString());
+        }
+
+        //send packet to /email-verification to validate JWT and update db entry
+        try {
+            const response = await fetch(buildPath('api/email-verification'), { method: 'POST', body: packetJSON, headers: { 'Content-Type': 'application/json' } });
+
+            const result = JSON.parse(await response.text());
+            if (!(result.error == '')) { //empty error = success
+                setMessage('Verification failed: ' + result.error);
+                return;
+            }
+
+            setMessage('Your email has been verified!');
+        } catch (error: any) {
+            setMessage(error.toString());
+        }
+    };
+
     return (
-        <div>
-            <EmailVerify />
+        <div id="verifyDiv">
+            <span id="inner-title">Verifying your email...</span><br /><br />
+            <span id="verifyResult">{message}</span>
         </div>
-    );
-};
+    )
+}
 
 export default VerifyPage;
